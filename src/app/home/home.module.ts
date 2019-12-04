152487f3a42d3fdcc97b4d9de4232d063f890d5b
Midas Wouters <midas.wouters@gmail.com>
2019-12-04 20:12:18 +0100

    Home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MatIconModule } from '@angular/material';
 
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ]
 })
 export class HomeModule { }
